59e1300f474a5ade1292512413d55158d0b053ae
subhadarshani <subhadarshani82@gmail.com>
2025-08-19 00:09:40 +0530

    create the Employees model ,register in admin.py and apply migrations.

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Employees
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=['id','name','email','contact']
+    
+admin.site.register(Employees,EmployeeAdmin)
--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Employees(models.Model):
+    name=models.CharField(max_length=30)
+    email=models.EmailField(max_length=50)
+    contact=models.IntegerField()
+    
+    
+    def __str__(self):
+        return self.name
