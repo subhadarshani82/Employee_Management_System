4fbdaf7132f70ccc83730eb362b30a3ab0a2ce9a
subhadarshani <subhadarshani82@gmail.com>
2025-08-19 23:11:53 +0530

    impliment finctionality for search employee.

--- employees/views.py
+++ employees/views.py
@@ -2,35 +2,55 @@ from django.shortcuts import render,redirect
 from . models import Employees
 from django.contrib import messages
 import re
+from django.db.models import Q
 # Create your views here.
 
 def list_employee(request):
-    employees=Employees.objects.all()
-    print(employees)
-    return render(request,'list_employees.html',{'employees':employees})
+    query=request.GET.get('q',"").strip()
+    edit_id=request.GET.get('edit')
+    # print(query)
+    employees=Employees.objects.all() 
+    # print(employees)
+    if 'q' in request.GET:
+        if query=="":
+            messages.error(request,'Please provide value to search.',extra_tags='search')
+        else:
+            employees= Employees.objects.filter(
+                Q(name__icontains=query)| Q(email__icontains=query)| Q(contact__icontains=query)
+            )
+            if not employees.exists():
+                messages.warning(request,'No matching Employee found.',extra_tags='search')
+    employee=None
+    if edit_id:
+        try:
+            employee= Employees.objects.get(id=edit_id)
+        except Employees.DoesNotExist:
+            messages.error(request,'Employee not found.',extra_tags='modal')
+    return render(request,'list_employees.html',{'employees':employees,'employee':employee})
     
 def add_employee(request):
     if request.method=='POST':
         name=request.POST.get('name').strip()
         email=request.POST.get('email').strip()
-        phone=request.POST.get('contact').strip()
+        mobile=request.POST.get('contact').strip()
         
         #validation 
         if not name or not re.fullmatch(r'[A-Za-z ]+',name):
-            messages.error(request,"Name can not be blank and it should contain only Alphabets and Space.")
-        elif not name or not re.fullmatch(r'[^@]+@[^@]+\.[^@]+',email):
-            messages.error(request,"Email should be valid email.")
-        elif not phone.isdigit() or len(phone)!=10 :
-            messages.error(request,"Mobile no should be of 10 digits.")
+            messages.error(request,"Name can not be blank and it should contain only Alphabets and Space.",extra_tags='modal')
+            return redirect('/list_employee/?modal=add')
+        elif not email or not re.fullmatch(r'[^@]+@[^@]+\.[^@]+',email):
+            messages.error(request,"Email should be valid email.",extra_tags='modal')
+            return redirect('/list_employee/?modal=add')
+        elif not mobile.isdigit() or len(mobile)!=10 :
+            messages.error(request,"Mobile no should be of 10 digits.",extra_tags='modal')
+            return redirect('/list_employee/?modal=add')
         else:
-            print(name,email,phone)
-            employees=Employees(name=name,email=email,contact=phone)
+            print(name,email,mobile)
+            employees=Employees(name=name,email=email,contact=mobile)
             employees.save()
-            messages.success(request,"Employee added Successfully.")
-        return redirect('list_employee')
-        # return redirect(f'/list_employee/?modal=add')
-        
-    # return render(request,'list_employees.html')
+            messages.success(request,"Employee added Successfully.",extra_tags='modal')
+            return redirect('/list_employee/?modal=add')
+
     return redirect('list_employee')
 
 def update_employee(request,id):
@@ -38,26 +58,29 @@ def update_employee(request,id):
     if request.method=='POST':
         name=request.POST.get('name').strip()
         email=request.POST.get('email').strip()
-        phone=request.POST.get('contact').strip()
+        mobile=request.POST.get('contact').strip()
         
         #validation 
         if not name or not re.fullmatch(r'[A-Za-z ]+',name):
-            messages.error(request,"Name can not be blank and it should contain only Alphabets and Space.")
+            messages.error(request,"Name can not be blank and it should contain only Alphabets and Space.",extra_tags='modal')
+            return redirect(f'/list_employee/?edit={id}')
         elif not name or not re.fullmatch(r'[^@]+@[^@]+\.[^@]+',email):
-            messages.error(request,"Email should be valid email.")
-        elif not phone.isdigit() or len(phone)!=10 :
-            messages.error(request,"Mobile no should be of 10 digits.")
+            messages.error(request,"Email should be valid email.",extra_tags='modal')
+            return redirect(f'/list_employee/?edit={id}')
+        elif not mobile.isdigit() or len(mobile)!=10 :
+            messages.error(request,"Mobile no should be of 10 digits.",extra_tags='modal')
+            return redirect(f'/list_employee/?edit={id}')
         else:
             
             employee.name=name
             employee.email=email
-            employee.contact=phone
+            employee.contact=mobile
             employee.save()
-            messages.success(request,f"Employee {employee.name} updated Successfully.")
-        return redirect('list_employee')
-        # return redirect(f'/list_employee/?modal=update&emp_id={id}')
+            messages.success(request,f"Employee {employee.name} updated Successfully.",extra_tags='modal')
+            return redirect(f'/list_employee/?edit={id}')
+            
         
-    
+
     return redirect('list_employee')
 
 def delete_employee(request,id):
