20c210dc9d0b35f948d3e18969b343e698454575
subhadarshani <subhadarshani82@gmail.com>
2025-08-19 13:15:51 +0530

    create add employee functionality with validation.

--- employees/models.py
+++ employees/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Employees(models.Model):
     name=models.CharField(max_length=30)
-    email=models.EmailField(max_length=50)
-    contact=models.IntegerField()
+    email=models.EmailField()
+    contact=models.CharField(max_length=10)
     
     
     def __str__(self):
--- employees/views.py
+++ employees/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . models import Employees
+from django.contrib import messages
+import re
 # Create your views here.
 def add_employee(request):
     if request.method=='POST':
-        name=request.POST.get('name')
-        email=request.POST.get('email')
-        phone=request.POST.get('contact')
-        print(name,email,phone)
+        name=request.POST.get('name').strip()
+        email=request.POST.get('email').strip()
+        phone=request.POST.get('contact').strip()
+        
+        #validation 
+        if not name or not re.fullmatch(r'[A-Za-z ]+',name):
+            messages.error(request,"Name can not be blank and it should contain only Alphabets and Space.")
+        elif not name or not re.fullmatch(r'[^@]+@[^@]+\.[^@]+',email):
+            messages.error(request,"Email should be valid email.")
+        elif not phone.isdigit() or len(phone)!=10 :
+            messages.error(request,"Mobile no should be of 10 digits.")
+        else:
+            print(name,email,phone)
+            employees=Employees(name=name,email=email,contact=phone)
+            employees.save()
+            messages.success(request,"Employee added Successfully.")
+        return render(request,'list_employees.html')
+        # return redirect('add_employee')#change it later
+        
     return render(request,'list_employees.html')
